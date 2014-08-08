6d2b046d96a9d1375810aecd8e72d0d966fb6b6a
ryu <1025ryu@naver.com>
2014-08-08 22:58:46 +0900

    8/8과제 최종본

--- application/controllers/index.py
+++ application/controllers/index.py
@@ -15,9 +15,11 @@ def login():
 		password=request.form['password']
 		if user_manager.login_check(email,password):
 			session['email']=request.form['email']
-			wall_id=user_manager.get_user_list(email)
+			wall_id=user_manager.get_user_list(email).id
+			username=user_manager.get_user_list(email).username
+			session['username']=username
 			session['wall_id']=wall_id
-			return render_template('timeline.html',wall_id=wall_id)
+			return render_template('timeline.html',wall_id=wall_id,username=username)
 		else:
 			return render_template('login.html')
 	else:
@@ -31,15 +33,20 @@ def signup():
 	else:
 		return render_template('signup.html')
 
-@app.route('/write')
+@app.route('/write',methods=['GET','POST'])
 def write():
-	return render_template('write.html')
+	if request.method=="POST":
+		posts=create(request.form['post'])
+		return render_template('timeline.html',posts=posts)
+	else:
+		return render_template('write.html')
 
 @app.route('/',defaults={'wall_id':0})
 @app.route('/timeline/<int:wall_id>')
 def timeline(wall_id):
-	session['wall_id']=wall_id
-	return render_template('timeline.html',message=wall_id)
+	username=user_manager.get_username(wall_id).username
+	session['username']=username
+	return render_template('timeline.html',message=username)
 
 @app.route('/read')
 def read():
--- application/models/user_manager.py
+++ application/models/user_manager.py
@@ -16,7 +16,18 @@ def add_user(data):
 
 def get_user_list(mail):
 	user=User.query.filter_by(email=mail).first()
-	return user.id
-
+	return user
+def get_username(id):
+	return User.query.get(id)
 def login_check(email,password):
 	return User.query.filter(User.email==email,User.password==db.func.md5(password)).count()!=0
+
+def create(text):
+	post=Post(
+		user_id=session['username'],
+		wall_id=session['wall_id'],
+		body=text,
+		is_secret='1'
+		)
+	db.session.add(post)
+	db.session.commit()
